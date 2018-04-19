ac57f34023a328cf0b3592dfeec81edacc5520f2
luvemil <taran.marco@gmail.com>
2018-04-19 14:11:46 +0200

    Move unicode mode selection to setup layer

--- keyboards/dz60/keymaps/luvemil/keymap.c
+++ keyboards/dz60/keymaps/luvemil/keymap.c
@@ -8,17 +8,20 @@ enum {
 };
 
 // My Keycodes
-enum {
+enum my_keycodes {
   _BASE = 0,
   _FUN,
   _MACRO,
-  _SETUP
+  _SETUP,
+  UNIWIN = SAFE_RANGE,
+  UNILIN,
+  UNIMAC
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_BASE] = LAYOUT(
-  F(0), KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
+  KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
   LT(_FUN,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
   KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
   KC_LSFT, KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(_FUN),
@@ -40,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_SETUP] = LAYOUT(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______,  UNIWIN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______,  UNILIN, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______,  UNIMAC, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 };
 
@@ -85,6 +88,29 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
   }
 }
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case UNIWIN:
+      if (record->event.pressed) {
+        set_unicode_input_mode(UC_WINC);
+      }
+      return false;
+      break;
+    case UNILIN:
+      if (record->event.pressed) {
+        set_unicode_input_mode(UC_LNX);
+      }
+      return false;
+      break;
+    case UNIMAC:
+      if (record->event.pressed) {
+        set_unicode_input_mode(UC_OSX);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Ctrl, twice for Alt
