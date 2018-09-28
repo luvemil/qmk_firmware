fe6b3ab249a388f501e01842e71a3cc4c697b21a
Marco Tarantino <taran.marco@gmail.com>
2018-09-28 18:41:54 +0200

    Add a layer without hold functions on Space and Tab for gaming

--- keyboards/dz60/keymaps/luvemil/keymap.c
+++ keyboards/dz60/keymaps/luvemil/keymap.c
@@ -17,6 +17,7 @@ enum my_keycodes {
   _SETUP,
   _MACRO,
   _NUMPAD,
+  _GAMING,
   UNIWIN = SAFE_RANGE,
   UNILIN,
   UNIMAC,
@@ -53,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SETUP] = LAYOUT(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
   _______, _______,  UNIWIN, _______, TG(_MACRO), _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______,  UNILIN, _______, _______, _______,
+  _______, _______, _______, _______, _______, TG(_GAMING), _______, _______, _______,  UNILIN, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, TG(_NUMPAD), UNIMAC, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
@@ -70,6 +71,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX, XXXXXXX, KC_ENT,
   KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_0, KC_COMM, KC_DOT, XXXXXXX, KC_BSPC, KC_DEL,
   KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, TD(TD_RCTRL_RALT), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT),
+
+[_GAMING] = LAYOUT(
+  KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
+  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+  KC_LSFT, KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, TG(_GAMING),
+  KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_SPC, KC_NO, TD(TD_RCTRL_RALT), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT),
 
 };
 
